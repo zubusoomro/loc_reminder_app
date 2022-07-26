1f1262bb79d1a208ae301cf71fa7c07280fb48cb
Zubair Ahmed Soomro <zubusoomro@gmail.com>
2022-07-27 02:05:46 +0500

    upgraded pubspec yaml + main file

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:get/get.dart';
 
 import 'routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     systemStatusBarContrastEnforced: true,
@@ -22,10 +24,7 @@ void main() {
             elevation: 0,
             color: Colors.black,
             centerTitle: true,
-            titleTextStyle: TextStyle(
-                color: Color.fromARGB(255, 253, 186, 71),
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+            titleTextStyle: TextStyle(color: Color.fromARGB(255, 253, 186, 71), fontSize: 20, fontWeight: FontWeight.bold),
             iconTheme: IconThemeData(color: Color.fromARGB(255, 253, 186, 71))),
         scaffoldBackgroundColor: Colors.black,
         primaryColor: const Color.fromARGB(255, 253, 186, 71),
@@ -35,14 +34,11 @@ void main() {
         shadowColor: const Color.fromARGB(216, 29, 29, 32),
         errorColor: const Color(0xFFB00020),
         secondaryHeaderColor: const Color.fromARGB(255, 133, 86, 4),
-        buttonTheme: const ButtonThemeData(
-            buttonColor: Color.fromARGB(255, 253, 143, 139),
-            textTheme: ButtonTextTheme.primary),
+        buttonTheme: const ButtonThemeData(buttonColor: Color.fromARGB(255, 253, 143, 139), textTheme: ButtonTextTheme.primary),
         inputDecorationTheme: InputDecorationTheme(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-                color: Color.fromARGB(255, 255, 255, 255), width: 2.0),
+            borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255), width: 2.0),
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -71,10 +67,7 @@ void main() {
             elevation: 0,
             color: Colors.white,
             centerTitle: true,
-            titleTextStyle: TextStyle(
-                color: Color.fromARGB(255, 113, 74, 142),
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+            titleTextStyle: TextStyle(color: Color.fromARGB(255, 113, 74, 142), fontSize: 20, fontWeight: FontWeight.bold),
             iconTheme: IconThemeData(color: Color.fromARGB(255, 113, 74, 142))),
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color.fromARGB(255, 113, 74, 142),
@@ -84,9 +77,7 @@ void main() {
         shadowColor: const Color.fromARGB(255, 236, 236, 236),
         errorColor: const Color.fromARGB(255, 212, 0, 39),
         secondaryHeaderColor: const Color.fromARGB(255, 103, 46, 146),
-        buttonTheme: const ButtonThemeData(
-            buttonColor: Color.fromARGB(255, 253, 143, 139),
-            textTheme: ButtonTextTheme.primary)),
+        buttonTheme: const ButtonThemeData(buttonColor: Color.fromARGB(255, 253, 143, 139), textTheme: ButtonTextTheme.primary)),
     debugShowCheckedModeBanner: false,
   ));
 }
