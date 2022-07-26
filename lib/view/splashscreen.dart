ef32123f5c75367e273016612ea484a736762f94
iamzaidsoomro <zaid3614@gmail.com>
2022-07-26 21:47:05 +0500

    Splashscreen

---
+++ lib/view/splashscreen.dart
@@ -0,0 +1,27 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loc_reminder_app/screens/get_started.dart';
+
+class SplashScreen extends GetWidget {
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: const Image(
+          image: AssetImage("assets/images/splashscreen_image.png")),
+      durationInSeconds: 5,
+      navigator: const GetStarted(),
+      logoSize: MediaQuery.of(context).size.width * 0.4,
+      title: Text(
+        "Loc Reminder",
+        style: TextStyle(
+          fontSize: MediaQuery.of(context).size.width * 0.05,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      showLoader: false,
+      backgroundColor: Color.fromARGB(255, 20, 0, 35),
+    );
+  }
+}
