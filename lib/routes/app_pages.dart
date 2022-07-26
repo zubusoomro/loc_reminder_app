ee98babb81e48527833f00238d5e9c6a19408358
iamzaidsoomro <zaid3614@gmail.com>
2022-07-26 21:46:39 +0500

    Route added for splashscreen

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -4,11 +4,13 @@ import 'package:loc_reminder_app/routes/routes.dart';
 import 'package:loc_reminder_app/screens/get_started.dart';
 import 'package:loc_reminder_app/view/login_view.dart';
 import 'package:loc_reminder_app/view/signup_view.dart';
+import 'package:loc_reminder_app/view/splashscreen.dart';
 
 class AppPages {
   AppPages._();
-  static const INITIAL_ROUTE = Routes.getStarted;
+  static const INITIAL_ROUTE = Routes.splashscreen;
   static final routes = [
+    GetPage(name: Routes.splashscreen, page: () => SplashScreen()),
     GetPage(name: Routes.getStarted, page: () => const GetStarted()),
     GetPage(
         name: Routes.login,
