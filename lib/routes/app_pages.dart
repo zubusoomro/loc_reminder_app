bce9b5791245cd22345d4c752ed17d707fa443b8
Zubair Ahmed Soomro <zubusoomro@gmail.com>
2022-08-08 02:58:24 +0500

    Update app_pages.dart

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,25 +1,37 @@
 import 'package:get/get.dart';
-import 'package:loc_reminder_app/binder/login_controller_binding.dart';
 import 'package:loc_reminder_app/routes/routes.dart';
-import 'package:loc_reminder_app/view/get_started.dart';
-import 'package:loc_reminder_app/view/homepage.dart';
-import 'package:loc_reminder_app/view/login_view.dart';
-import 'package:loc_reminder_app/view/notes_view.dart';
-import 'package:loc_reminder_app/view/signup_view.dart';
-import 'package:loc_reminder_app/view/splashscreen.dart';
+import 'package:loc_reminder_app/screens/get_started.dart';
+import 'package:loc_reminder_app/screens/home/homepage.dart';
+import 'package:loc_reminder_app/screens/home/homepage_controller.dart';
+import 'package:loc_reminder_app/screens/login/login_controller.dart';
+import 'package:loc_reminder_app/screens/login/login_view.dart';
+import 'package:loc_reminder_app/screens/notes_view.dart';
+import 'package:loc_reminder_app/screens/signup_view.dart';
+import 'package:loc_reminder_app/screens/splashscreen.dart';
 
-import '../view/notes_create.dart';
+import '../screens/notes_create.dart';
 
 class AppPages {
   AppPages._();
+
   static const initialRoute = Routes.splashscreen;
   static final routes = [
     GetPage(name: Routes.splashscreen, page: () => const SplashScreen()),
     GetPage(name: Routes.getStarted, page: () => const GetStarted()),
-    GetPage(name: Routes.login, page: () => const LoginView(), binding: LoginControllerBinding()),
-    GetPage(name: Routes.signup, page: () => SignupView()),
-    GetPage(name: Routes.main, page: () => Homepage()),
-    GetPage(name: Routes.notes, page: () => NotesView()),
-    GetPage(name: Routes.createNotes, page: () => CreateNotesView()),
+    GetPage(
+        name: Routes.login,
+        page: () => const LoginView(),
+        binding: BindingsBuilder(() {
+          Get.lazyPut(() => LoginController());
+        })),
+    GetPage(name: Routes.signup, page: () => const SignupView()),
+    GetPage(
+        name: Routes.main,
+        page: () => Homepage(),
+        binding: BindingsBuilder(() {
+          Get.lazyPut<HomePageController>(() => HomePageController());
+        })),
+    GetPage(name: Routes.notes, page: () => const NotesView()),
+    GetPage(name: Routes.createNotes, page: () => const CreateNotesView()),
   ];
 }
